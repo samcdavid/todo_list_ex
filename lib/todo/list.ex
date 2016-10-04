36fdff69f90307d1e92ab56cbe744880c97dc360
Sam McDavid <mcdavidsa@gmail.com>
2016-10-04 08:18:33 -0400

    Basic Todo List Module
    Why
    ---
    
    - Users need a way of creating and obtaining todo list entries.
    
    This change addresses the need by
    ---------------------------------
    
    - `add_entry/3` function for creating todo list items.
    - `entries/2` function for retrieving todo list items for a specific
    date key.

--- lib/todo/list.ex
+++ lib/todo/list.ex
@@ -1,3 +1,16 @@
 defmodule Todo.List do
   def new, do: HashDict.new
+
+  def add_entry(todo_list, date, title) do
+    HashDict.update(
+      todo_list,
+      date,
+      [title],                            # initial value
+      fn(titles] -> [title | titles] end  # lamda to update the value
+    )
+  end
+
+  def entries(todo_list, date) do
+    HashDict.get(todo_list, date, [])
+  end
 end
